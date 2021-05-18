63299c2aef0a64262046d87856bd5f15e6731ef7
Jorge <jorge.deandres@hotmail.es>
2021-05-19 00:58:55 +0200

    First commit with structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # python-library-template
-Template for creating python libraries
+
+Template for creating python libraries easily.
+
+## Why do I need this folders?
+
+TODO
+## What are the steps I need to follow in order to create a new library?
+
+TODO
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='python_lib_name',
+    packages=find_packages(include=['python_lib_name']),
+    version='0.1.0',
+    description='My Python library',
+    author='Me',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests'
+)
