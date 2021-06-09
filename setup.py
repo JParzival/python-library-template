44911e179207ed1f2ba494ece97bfe506fb9274a
Jorge <jorge.deandres@hotmail.es>
2021-06-09 21:03:57 +0200

    Update setup.py to find packages

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='python_lib_name',
-    packages=find_packages(include=['python_lib_name']),
+    packages=find_packages(),
     version='0.1.0',
     description='My Python library',
     author='Me',
